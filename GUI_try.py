7966db3943940ac86066b34a5050ed57325288ef
flowerup <sharfinabila15@gmail.com>
2025-06-03 22:38:09 +0700

    GUI_try.py

--- GUI_try.py
+++ GUI_try.py
@@ -118,11 +118,17 @@ st.markdown("""
 # Tombol navigasi sejajar
 col1, col2 = st.columns(2)
 with col1:
+    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
     if st.button("Page 1"):
         go_to_page("page1")
+    st.markdown("</div>", unsafe_allow_html=True)
+
 with col2:
+    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
     if st.button("Page 2"):
         go_to_page("page2")
+    st.markdown("</div>", unsafe_allow_html=True)
+
 
 # Konten halaman
 if st.session_state.page == "home":
